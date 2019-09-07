8d3cf368baaf4b3eb2b65a589099a22271c31830
Benoit TANI <benoit.tani@outlook.com>
2019-09-07 14:39:30 +0800

    Integrate Coupon Bazaar code from Waves training.

--- README.md
+++ README.md
@@ -1,9 +1,2 @@
 # mweb3waves
-Mastering Web3 with Waves.
-
-Demo: https://netsyde-mweb3waves.herokuapp.com/
-
-# How to install:
-* yarn or npm install
-* yarn build or npm run build:dev
-* node server.js
+Tmp demo Mastering Web3 with Waves app
--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.get('/', (req, res) => {
-	res.sendFile('index.html', { root : __dirname});
-});
+app.use(express.static(`${__dirname}/public/`));
 
-let port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.sendFile('/public/index.html', { root: __dirname });
+});
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-	console.log(__dirname);
-	console.log(`Listening Port ${port}.`);
+    console.log(__dirname);
+    console.log(`Listening Port ${port}`);
 });
